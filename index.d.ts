780be1297b15a7626a531935f1439b5803d7e817
Wladimir Zhosan <user@192-168-0-100.rdsnet.ro>
2019-11-06 21:56:06 +0200

    Added types for request callback function

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,18 @@
-export function useRequest<T>(options?: {
-  data?: any;
-  errors?: { [key: string]: string };
-}): {
-  request: (fn: any, options?: { concat?: boolean | string }) => Promise<T>;
+interface RequestResponse<T> {
+  request: (
+    fn: { data: T },
+    options?: {
+      concat?: boolean | string;
+    },
+  ) => Promise<T>;
   data: T;
   errors: { [key: string]: string };
   loading: boolean;
-};
+}
+
+interface RequestOptions {
+  data?: any;
+  errors?: { [key: string]: string };
+}
+
+export function useRequest<T>(options?: RequestOptions): RequestResponse<T>;
